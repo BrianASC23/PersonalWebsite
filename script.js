621646e15a222c6fcefbe656025ef95a197044aa
BrianASC23 <bcfrags@gmail.com>
2023-08-06 00:01:26 -0400

    third commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-//Navigating through Bars
+//Recommendations Page Submission
+//Whenever the input is submitted, it is pushed to realArray
+//The input is also shown as a list below the submit button
+let recInput = document.getElementById("recinput");
+let recBtn = document.getElementById("rec_btn");
+let recArray = document.getElementById("recArray");
+let realArray = [];
+let para;
 
+recBtn.onclick = function(event) {
+    event.preventDefault();
 
+    if(recInput.value == ""){
+        alert("Please fill in the box to leave a recommendation.")
+    } else {
+        para = document.createElement("li");
+        para.innerHTML += recInput.value
+        recArray.appendChild(para);
+
+        realArray.push(recInput.value);
+    }
+}
 
 
 
